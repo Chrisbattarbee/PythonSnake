eccb67f19ea63112bdb6808061dee5a0b6711e7e
Chris Battarbee <chrisbattarbee@chriss-mbp.home>
2016-09-21 15:00:08 +0100

    Refactor to pep8

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import pygame
 import sys
 import random
 
+
 class Node:
-    def __init__(self, initPosition, imageLocation, screenToRenderOn):
-        self.position = initPosition
-        self.nodeBody = pygame.image.load(imageLocation)
+    def __init__(self, init_position, image_location, screen_to_render_on):
+        self.position = init_position
+        self.nodeBody = pygame.image.load(image_location)
         self.nodeBodyRect = self.nodeBody.get_rect()
-        self.screen = screenToRenderOn
+        self.screen = screen_to_render_on
         self.speed = [0, 0]
 
-    def updatePosition(self):
+    def update_position(self):
         self.position[0] += self.speed[0]
         self.position[1] += self.speed[1]
         self.nodeBodyRect.left = self.position[0]
@@ -19,128 +20,130 @@ class Node:
     def render(self):
         screen.blit(self.nodeBody, self.nodeBodyRect)
 
+
 class SnakeBody:
-    def __init__(self, initPosition, imageLoc, screenToRenderOn):
-        self.imageLocation = imageLoc
-        self.screen = screenToRenderOn
-        self.nodes = [Node(initPosition, self.imageLocation, self.screen)]
+    def __init__(self, init_position, image_loc, screen_to_render_on):
+        self.imageLocation = image_loc
+        self.screen = screen_to_render_on
+        self.nodes = [Node(init_position, self.imageLocation, self.screen)]
 
     def render(self):
         for node in self.nodes:
             node.render()
 
-    def updateSpeed(self, speedOfFirst):
+    def update_speed(self, speed_of_first):
 
         # We change the speed of the first node and then all of the rest of the nodes
         # take the speed of the node one before them in the list
         if len(self.nodes) != 1:
             for x in range(len(self.nodes) - 1, -1, -1):
                 self.nodes[x].speed = self.nodes[x - 1].speed
-        self.nodes[0].speed = speedOfFirst
+        self.nodes[0].speed = speed_of_first
 
-    def updatePositions(self):
+    def update_positions(self):
 
         # Update the position of all of the nodes using the speed that we have just changed
         for node in self.nodes:
-            node.updatePosition()
+            node.update_position()
 
-    def addNode(self, position):
+    def add_node(self, position):
         self.nodes.append(Node(position, self.imageLocation, self.screen))
 
-    def isInsideSelfOrOutsideRegion(self, width, height):
+    def is_inside_self_or_outside_region(self, wid, heigh):
 
-        isInside = False
+        is_inside = False
 
-        if self.nodes[0].position[0] > width - 12 or self.nodes[0].position[1] > height - 12 or self.nodes[0].position[0] < 0 or self.nodes[0].position[1] < 0:
-            isInside = True
+        if self.nodes[0].position[0] > wid - 12 or self.nodes[0].position[1] > heigh - 12 or \
+                self.nodes[0].position[0] < 0 or self.nodes[0].position[1] < 0:
+            is_inside = True
 
         for x in range(1, len(self.nodes)):
             if self.nodes[0].position == self.nodes[x].position:
-                isInside = True
+                is_inside = True
 
-        return isInside
+        return is_inside
+
 
 class Food:
-
-    def __init__(self, pos, imageLoc, screenToRenderOn):
-        self.foodBody = pygame.image.load(imageLoc)
+    def __init__(self, pos, image_loc, screen_to_render_on):
+        self.foodBody = pygame.image.load(image_loc)
         self.position = pos
         self.foodBodyRect = self.foodBody.get_rect()
         self.foodBodyRect.x = pos[0]
         self.foodBodyRect.y = pos[1]
-        self.screen = screenToRenderOn
+        self.screen = screen_to_render_on
 
     def render(self):
         self.screen.blit(self.foodBody, self.foodBodyRect)
 
+
 class FoodManager:
-
-    def __init__(self, imageLoc, screenToRenderOn, snakeObject):
-        self.imageLocation = imageLoc
-        self.screen = screenToRenderOn
+    def __init__(self, image_loc, screen_to_render_on, snake_object):
+        self.imageLocation = image_loc
+        self.screen = screen_to_render_on
         self.foodList = []
-        self.createFood(snakeObject)
+        self.create_food(snake_object)
 
-    def createFood(self, snake):
+    def create_food(self, snake_object):
 
-        isInsideSomething = True
         x = random.randint(1, 13) * 24  # X 24 to make it fit exactly to the grid
         y = random.randint(1, 9) * 24
         pos = [x, y]
 
-        while self.isInside(snake, pos) is True:
-            x = random.randint(1,13) * 24 # X 24 to make it fit exactly to the grid
-            y = random.randint(1,9) * 24
-            pos = [x,y]
+        while self.is_inside(snake_object, pos) is True:
+            x = random.randint(1, 13) * 24  # X 24 to make it fit exactly to the grid
+            y = random.randint(1, 9) * 24
+            pos = [x, y]
 
         self.foodList.append(Food(pos, self.imageLocation, self.screen))
 
-    def isInside(self, snake, pos):
+    def is_inside(self, snake_object, pos):
 
-        isInsideSomething = False
-        for node in snake.nodes:
+        is_inside_something = False
+        for node in snake_object.nodes:
             if node.position == pos:
-                isInsideSomething= True
+                is_inside_something = True
 
         for food in self.foodList:
             if food.position == pos:
-                isInsideSomething = True
+                is_inside_something = True
 
-        return isInsideSomething
+        return is_inside_something
 
-    def removeFoodByPos(self, pos):
+    def remove_food_by_pos(self, pos):
 
         # Find the food we need and delete
         for food in self.foodList:
             if pos == food.position:
                 self.foodList.remove(food)
 
-    def removeFoodByRef(self, food):
+    def remove_food_by_ref(self, food):
         self.foodList.remove(food)
 
     def render(self):
         for food in self.foodList:
             food.render()
 
+
 class GameManager:
-    def __init__(self, snakeObject, screenObject, foodManager, fontObject, wid, hig):
+    def __init__(self, snake_object, screen_object, food_manager, font_object, wid, hig):
         self.gameTick = 0
         self.addQueue = [0, 0]
         self.addQueueGameTick = 999999999
-        self.snake = snakeObject
-        self.screen = screenObject
+        self.snake = snake_object
+        self.screen = screen_object
         self.speed = [0, 0]
         self.state = 'right'
         self.prevState = 'up'
-        self.fm = foodManager
+        self.fm = food_manager
         self.isAlive = True
         self.score = 0
-        self.font = fontObject
+        self.font = font_object
         self.width = wid
         self.height = hig
 
-    def GameCycle(self):
-        if self.isAlive == True:
+    def game_cycle(self):
+        if self.isAlive is True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
@@ -157,34 +160,32 @@ class GameManager:
                         self.state = 'right'
 
             # Handles speed logic
-            currentSpeed = self.speed
+            current_speed = self.speed
 
-
-            self.screen.fill(black)
-            self.snake.updatePositions()
-            self.SetSpeed(currentSpeed)
+            self.screen.fill([0, 0, 0])
+            self.snake.update_positions()
+            self.set_speed(current_speed)
             if self.gameTick % 12 == 0:
-                self.checkIfFoodHasBeenEaten()
+                self.check_if_food_has_been_eaten()
 
-                #Check if the game should end
-                if self.snake.isInsideSelfOrOutsideRegion(width, height) is True:
+                # Check if the game should end
+                if self.snake.is_inside_self_or_outside_region(width, height) is True:
                     self.isAlive = False
 
             if self.gameTick % 100 == 0:
-                self.fm.createFood(self.snake)
+                self.fm.create_food(self.snake)
             self.fm.render()
             self.snake.render()
-            self.spawnNewNodes()
+            self.spawn_new_nodes()
 
             pygame.display.flip()
 
             self.gameTick += 1
         else:
-            self.renderScore()
+            self.render_score()
 
-    def SetSpeed(self, currentSpeed):
+    def set_speed(self, current_speed):
 
-        speed = [0, 0]
         if self.gameTick % 12 == 0:
 
             # Add all the new nodes
@@ -198,35 +199,33 @@ class GameManager:
             elif self.state == 'down' and self.prevState != 'up':
                 self.speed = [0, 2]
             else:
-                self.speed = currentSpeed
-            self.snake.updateSpeed(self.speed)
+                self.speed = current_speed
+            self.snake.update_speed(self.speed)
         else:
-            self.speed = currentSpeed
+            self.speed = current_speed
 
-    def spawnNewNodes(self):
+    def spawn_new_nodes(self):
         if self.gameTick == self.addQueueGameTick + 10:
-            self.snake.addNode(self.addQueue)
+            self.snake.add_node(self.addQueue)
             self.addQueueGameTick = 0
 
-    def AddNodeToQueue(self, position, gt):
+    def add_node_to_queue(self, position, gt):
         self.addQueue = position
         self.addQueueGameTick = gt
 
-    def checkIfFoodHasBeenEaten(self):
+    def check_if_food_has_been_eaten(self):
         for food in self.fm.foodList:
             if food.position == self.snake.nodes[0].position:
-
-                self.AddNodeToQueue(self.snake.nodes[len(self.snake.nodes) - 1].position[:], self.gameTick)
-                self.fm.removeFoodByRef(food)
+                self.add_node_to_queue(self.snake.nodes[len(self.snake.nodes) - 1].position[:], self.gameTick)
+                self.fm.remove_food_by_ref(food)
                 self.score += 1
 
-    def renderScore(self):
+    def render_score(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-
                     # Reinitialize all variables that we need to
                     self.snake = SnakeBody([0, 0], "Resources/square_white_24_ns.bmp", screen)
                     self.isAlive = True
@@ -235,12 +234,12 @@ class GameManager:
                     self.prevState = 'up'
 
         self.screen.fill(black)
-        deathText = font.render("Oh no you died!", 1, (255,255,255))
-        scoreText = font.render("Score: " + str(self.score), 1, (255,255,255))
-        restartText = font.render("SPACE to retry.", 1, (255,255,255))
-        self.screen.blit(deathText, (50, 50))
-        self.screen.blit(scoreText, (50, 100))
-        self.screen.blit(restartText, (50, 150))
+        death_text = font.render("Oh no you died!", 1, (255, 255, 255))
+        score_text = font.render("Score: " + str(self.score), 1, (255, 255, 255))
+        restart_text = font.render("SPACE to retry.", 1, (255, 255, 255))
+        self.screen.blit(death_text, (50, 50))
+        self.screen.blit(score_text, (50, 100))
+        self.screen.blit(restart_text, (50, 150))
         pygame.display.flip()
 
 
@@ -251,13 +250,11 @@ size = width, height = 312, 240
 
 screen = pygame.display.set_mode(size)
 
-snake = SnakeBody([0,0], 'Resources/square_white_24_ns.bmp', screen)
-
-black = [0, 0, 0]
+snake = SnakeBody([0, 0], 'Resources/square_white_24_ns.bmp', screen)
 
 foodManager = FoodManager('Resources/square_green.bmp', screen, snake)
 
 gm = GameManager(snake, screen, foodManager, font, width, height)
 
 while 1:
-    gm.GameCycle()
+    gm.game_cycle()
